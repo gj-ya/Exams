615f08bbd6e8e452fdb96be56a0c6d0ca0dfec10
gj-ya <116793097+gj-ya@users.noreply.github.com>
2023-02-07 14:15:58 +0900

    Add files via upload

---
+++ 2Arr-multiplication-tables/src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    /*
+     * 문제 : 구구단을 출력하는 프로그램을 작성하세요.
+     * 제약조건
+     * 1. 2차원 배열을 사용해야 하며 3개의 Class를 모두 사용해야 합니다.
+     * 2. 이중 For문을 사용해야 합니다.
+     * 기타. method의 return, parameter를 변경 하셔도 됩니다.
+     */
+    
+    public static void main(String[] args) throws Exception {
+        Test2 t2 = new Test2();
+        t2.m2();
+        
+    }
+}
---
+++ 2Arr-multiplication-tables/src/Test3.java
@@ -0,0 +1,8 @@
+public class Test3 {
+    
+    public int m3(int i, int j){
+        int a = i * j;
+        
+        return a;
+    }
+}
